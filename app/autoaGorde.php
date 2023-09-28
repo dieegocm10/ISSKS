6361bf485c367a4fe55aa06d65d7b660b58ebb99
Ander <ander@ander-Aspire-E1-531.(none)>
2023-09-28 19:11:06 +0200

    añadidas opciones del menu

---
+++ app/autoaGorde.php
@@ -0,0 +1,21 @@
+<?php
+  
+  $marka = $_POST['marka'];
+  $prezioa = $_POST['prezioa'];
+  $matrikula = $_POST['matrikula'];
+  $karburanteMota = $_POST['karburanteMota'];
+  $modeloa = $_POST['modeloa'];
+
+  $hostname = "db";
+  $username = "admin";
+  $password = "test";
+  $db = "database";
+
+  $conn = mysqli_connect($hostname, $username, $password, $db);
+  if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+  }
+
+  $query = mysqli_query($conn, "INSERT INTO AUTOA VALUES ('$marka', '$prezioa', '$matrikula', '$karburanteMota', '$modeloa')")
+    or die (mysqli_error($conn));
+?>
