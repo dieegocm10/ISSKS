ae186e3657489d882a17aa9166edc2927e0105a5
Ander <ander@ander-Aspire-E1-531.(none)>
2023-09-28 17:56:42 +0200

    guarda el erabiltzaile en la base de datos

--- app/erabiltzaileBerriaGorde.php
+++ app/erabiltzaileBerriaGorde.php
@@ -1,13 +1,22 @@
 <?php
-
-
-  $nombre = $_POST['izenAbizenak'];
-  echo "Nombre: " . $nombre . "<br>";
-  $dni = $_POST['nan'];
-  echo $dni;
-  $telefono = $_POST['telefonoa'];
-  $email = $_POST['email'];
-  $fechaNacimiento = $_POST['jaiotzeData'];
-  $contraseña = $_POST['gakoa'];
   
+  $izenAbizenak = $_POST['izenAbizenak'];
+  $nan = $_POST['nan'];
+  $telefonoa = $_POST['telefonoa'];
+  $email = $_POST['email'];
+  $jaiotzeData = $_POST['jaiotzeData'];
+  $gakoa = $_POST['gakoa'];
+  
+  $hostname = "db";
+  $username = "admin";
+  $password = "test";
+  $db = "database";
+
+  $conn = mysqli_connect($hostname, $username, $password, $db);
+  if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+  }
+
+  $query = mysqli_query($conn, "INSERT INTO ERABILTZAILEA VALUES ('$izenAbizenak', '$nan', '$gakoa', '$telefonoa', '$jaiotzeData', '$email')")//no coge la jaotze data
+    or die (mysqli_error($conn));
 ?>
