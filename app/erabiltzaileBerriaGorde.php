476739a38d9fae6d85172be8a15d2d03943aac60
Ander <ander@ander-Aspire-E1-531.(none)>
2023-09-28 17:20:10 +0200

    entrega_1

--- app/erabiltzaileBerriaGorde.php
+++ app/erabiltzaileBerriaGorde.php
@@ -4,6 +4,7 @@
   $nombre = $_POST['izenAbizenak'];
   echo "Nombre: " . $nombre . "<br>";
   $dni = $_POST['nan'];
+  echo $dni;
   $telefono = $_POST['telefonoa'];
   $email = $_POST['email'];
   $fechaNacimiento = $_POST['jaiotzeData'];
