03aea11b7c1ae4c2e2273ba995ddce16430a01a5
Maria <maria@maria-IdeaPad-3-15IIL05.(none)>
2023-10-21 13:19:14 +0200

    autoaSartu y menu bonitos

--- app/menu.php
+++ app/menu.php
@@ -1,25 +1,63 @@
 <?php
-
-	$izenAbizenak = $_GET['parametro1'];
-	$gakoa = $_GET['parametro2'];
-
-  
-	echo'
-		<html>
-			<head>
-				<title>MENU</title>
-			</head>
-			
-			<body>
-				<div align="center" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
-				<img src="irudiak/4.jpg" width="200" height="150">
-				<table>
-					<tr>
-						<td><a href="autoaSartu.html"><input type="button" name="sartu" value="AUTOA SARTU"></a></td>
-						<td><a href="datuakEditatu.php?parametro1=';echo $izenAbizenak;echo'&parametro2=';echo $gakoa;echo'"><input type="button" name="sartuu" value="DATUAK EDITATU"></a></td>
-						<td><a href="datuakErakutsi.php"><input type="button" name="erregistratu" value="AUTOAK"></a></td>
-					</tr>
-				</table></div>
-			</body>
-		</html>';
+$izenAbizenak = $_GET['parametro1'];
+$gakoa = $_GET['parametro2'];
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MENU</title>
+    <style>
+        body {
+            background-color: #BAE1E0; 
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+        .image {
+            margin-bottom: 20px;
+        }
+        .comment {
+            background-color: #4285f4; 
+            color: #fff; 
+            padding: 10px;
+            font-size: 24px;
+            position: relative;
+            top: -50px; 
+        }
+        .button-container {
+            display: flex;
+            justify-content: center;
+        }
+        .button {
+            background-color: #4285f4; 
+            color: #fff; 
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            margin: 0 10px;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="comment">Hauetako bat hautatu:</div>
+        <img class="image" src="irudiak/4.jpg" width="400" height="350" alt="Imagen de muestra">
+        <div class="button-container">
+            <a class="button" href="autoaSartu.html">AUTOA SARTU</a>
+            <a class="button" href="datuakEditatu.php?parametro1=<?= $izenAbizenak ?>&parametro2=<?= $gakoa ?>">DATUAK EDITATU</a>
+            <a class="button" href="datuakErakutsi.php">AUTOAK</a>
+        </div>
+    </div>
+</body>
+</html>
+
+
