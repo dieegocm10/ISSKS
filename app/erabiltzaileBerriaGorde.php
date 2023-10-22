588d59c4b57510d8ac48bd2bf9539154a00feed1
Ander2A <alguiendeaqui78@gmail.com>
2023-10-22 18:37:04 +0200

    a

--- app/erabiltzaileBerriaGorde.php
+++ app/erabiltzaileBerriaGorde.php
@@ -15,6 +15,7 @@
   	if ($conn->connect_error) {
     		die("Database connection failed: " . $conn->connect_error);
   	}
+  	mysqli_set_charset($conn, "utf8mb4");
 
   	$query = mysqli_query($conn, "INSERT INTO ERABILTZAILEA VALUES ('$izenAbizenak', '$nan', '$gakoa', '$telefonoa', '$jaiotzeData', '$email')")
     	or die (mysqli_error($conn));
