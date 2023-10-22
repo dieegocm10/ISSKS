036b7194d4e5c374ac9882917d13393e13ca78c2
Urko-Aranda <urkoaragom@gmail.com>
2023-10-22 19:59:18 +0200

    Update autoaSartu.js

--- app/js/autoaSartu.js
+++ app/js/autoaSartu.js
@@ -1,8 +1,8 @@
 (function(){
     'use strict';
     document.addEventListener("DOMContentLoaded", function() {
-				var gordeBtn = document.getElementById("gorde");
-				var garbiBtn = document.getElementById("garbitu");
+				var gordeBtn = document.getElementById("gorde");	//"Gorde" botoia aldagai batean gorde
+				var garbiBtn = document.getElementById("garbitu");	//"Garbitu" botoia aldagai batean gorde
 				//BOTOI GORDE
 				gordeBtn.addEventListener("click", function() {
 					autoaGorde()
@@ -15,41 +15,41 @@
 				});
 			});
 			
-			function autoaGorde(){
-				var marka = document.getElementsByName("marka")[0].value;
-				var prezioa = document.getElementsByName("prezioa")[0].value;
-				var matrikula = document.getElementsByName("matrikula")[0].value;
-				var karburanteMota = document.getElementsByName("karburanteMota")[0].value;
-				var modeloa = document.getElementsByName("modeloa")[0].value;
+			function autoaGorde(){	//"Gorde" botoian klikatzerakoan: 
+				var marka = document.getElementsByName("marka")[0].value;	//Markaren balioa aldagai batean gorde	
+				var prezioa = document.getElementsByName("prezioa")[0].value;	//Prezioaren balioa aldagai batean gorde
+				var matrikula = document.getElementsByName("matrikula")[0].value;	//Matrikularen balioa aldagai batean gorde
+				var karburanteMota = document.getElementsByName("karburanteMota")[0].value;	//Karburante motaren balioa aldagai batean gorde
+				var modeloa = document.getElementsByName("modeloa")[0].value;	//Modeloaren balioa aldagai batean gorde
 
-				if (marka == "") {
+				if (marka == "") {	//Konprobatu marka hutsik badago
 					alert("MARKA ez da jarri");
-				  	return false;
+				  	return false;	//Hutsik badago alerta eman eta false bueltatu
 				}
 
-				if (prezioa == "") {
+				if (prezioa == "") {	//Konprobatu prezioa hutsik badago
 					alert("PREZIOA ez da jarri");
-				  	return false;
+				  	return false;	//Hutsik badago alerta eman eta false bueltatu
 				}
 
-				if (matrikula == "") {
+				if (matrikula == "") {	//Konprobatu matrikula hutsik badago
 					alert("MATRIKULA ez da jarri");
-				  	return false;
+				  	return false;	//Hutsik badago alerta eman eta false bueltatu
 				}
-				else{
+				else{	//Konprobatu matrikularen formatoa egokia dela
 					var patroia = /^\d{4} [A-Z]{3}$/;
 				    	if (!patroia.test(matriKula)) {
 				        	alert("MATRIKULA 1234 ABC formatua izan behar du");
-				        	return false;
+				        	return false;	//Egokia ez bada alerta eman eta false bueltatu
 				    	}
 				}
-				if (karburanteMota == "") {
+				if (karburanteMota == "") {	//Konprobatu Karburante mota hutsik badago
 					alert("KARBURANTE MOTA ez da jarri");
-				  	return false;
+				  	return false;	//Hutsik badago alerta eman eta false bueltatu
 				}
-				if (modeloa == "") {
+				if (modeloa == "") {	//Konprobatu modeloa hutsik badago
 					alert("MODELOA ez da jarri");
-				  	return false;
+				  	return false;	//Hutsik badago alerta eman eta false bueltatu
 				}
 
 				//DATUAK BIDALI
