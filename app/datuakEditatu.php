61fc7947f7ec37cc58c566bbd61e838b31edb89e
Urko <urkoaragom@gmail.com>
2023-11-12 13:08:41 +0100

    a

--- app/datuakEditatu.php
+++ app/datuakEditatu.php
@@ -89,8 +89,7 @@
 			    	var nan = document.getElementsByName("nan")[0].value; //NAN balioa aldagai batean gorde	
 			    	var telefonoa = document.getElementsByName("telefonoa")[0].value; //Telefono balioa aldagai batean gorde	
 			    	var jaiotzeData = document.getElementsByName("jaiotzeData")[0].value; //Jaiotze dataren balioa aldagai batean gorde	
-			    	var email = document.getElementsByName("email")[0].value; //Email-aren balioa aldagai batean gorde	
-			    	var gakoa = document.getElementsByName("gakoa")[0].value; //Gakoaren balioa aldagai batean gorde	
+			    	var email = document.getElementsByName("email")[0].value; //Email-aren balioa aldagai batean gorde		
 			    	if (izenAbizenak == "") { //Konprobatu IzenAbizenak hutsik badago
 					alert("IZEN ABIZENAK ez dira jarri");
 					return false; //Hutsik badago alerta eman eta false bueltatu
@@ -118,17 +117,6 @@
 				    		return false; //Egokia ez bada alerta eman eta false bueltatu
 					}
 			    	}
-			    	if (gakoa == "") { //Konprobatu gakoa hutsik badago
-					alert("GAKOA ez da jarri"); //Hutsik badago alerta eman eta false bueltatu
-					return false;
-			    	}
-			    	else{	//Konprbatu gakoa formatua egokia dela
-					var regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[a-z])[[a-zA-Z0-9.-]{6,}$/;
-					if(!regex.test(gakoa)){
-						alert("GAKOA ahula da, letra larriak, xeheak, zenbakiak eta gutxienez 6 karakterezko luzeera eduki behar du");
-						return false;	//Egokia ez bada alerta eman eta false bueltatu
-					}
-				}
 			}
 		</script>
 	</body>
